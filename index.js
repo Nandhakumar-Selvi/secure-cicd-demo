c4a48e1c45defb032a7392be152afc555d487e2a
Nandhakumar R <nandhakumarnandha34@gmail.com>
2026-07-01 10:58:43 +0530

    Trigger pipeline

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req,res)=>{
-    res.send("Secure CI/CD Demo");
+app.get("/", (req, res) => {
+  res.send("Secure CI/CD Demo");
 });
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
